bbcdc335edc4561d7d64517e999c664acd079163
Justinxtyme <binning.justin@gmail.com>
2025-09-12 15:28:13 -0700

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -1,1 +1,9 @@
 //
+
+#include <unordered_map>
+
+std::unordered_map<ItemID, Item> item_db = {
+    {ItemID::Potion,  {ItemID::Potion,  "Potion",  0.5f}},
+    {ItemID::HiPotion,{ItemID::HiPotion,"Hi-Potion", 0.7f}},
+    {ItemID::Elixir,  {ItemID::Elixir,  "Elixir", 1.0f}}
+};
